41a59490dacb53d3a62ad5d71d3e23b0633ff684
hrj <Hu13231012910@163.com>
2019-03-20 08:52:25 +0800

    第七单元技能作品

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function define() {
+    //定义模块
+    // require.define({
+
+    // })
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+function require() {
+    //导出模块
+    require.config({
+        "index": "js/index"
+    })
+}
